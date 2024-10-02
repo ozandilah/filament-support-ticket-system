dfce415b79e0df661f029bdc16690aa03e3f1e92
masozan7 <fauzan.fadilah@epfootwear.com>
2024-10-02 13:02:24 +0700

    Model, Migration and Resource Ticket

---
+++ app/Filament/Resources/TicketResource.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Ticket;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\TicketResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\TicketResource\RelationManagers;
+use Filament\Tables\Columns\TextInputColumn;
+
+class TicketResource extends Resource
+{
+    protected static ?string $model = Ticket::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->columns(1)
+            ->schema([
+                TextInput::make('title')
+                    ->autofocus()
+                    ->required(),
+                Textarea::make('description')
+                    ->rows(3),
+                Select::make('priority')
+                    ->options(Ticket::PRIORITY)
+                    ->required(),
+                Select::make('status')
+                    ->options(Ticket::STATUS)
+                    ->required(),
+                Select::make('assigned_to')
+                    ->relationship('assignedTo', 'name'),
+                Textarea::make('comment')
+                    ->rows(3),
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+
+                TextColumn::make('title')
+                    ->description(fn(Ticket $record): string => $record->description)
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('status')->badge(),
+                TextColumn::make('priority')->badge(),
+                TextColumn::make('assignedTo.name')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('assignedBy.name')
+                    ->searchable()
+                    ->sortable(),
+                TextInputColumn::make('comment')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTickets::route('/'),
+            'create' => Pages\CreateTicket::route('/create'),
+            'edit' => Pages\EditTicket::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/TicketResource/Pages/EditTicket.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Filament\Resources\TicketResource\Pages;
+
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+use App\Filament\Resources\TicketResource;
+
+class EditTicket extends EditRecord
+{
+    protected static string $resource = TicketResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [];
+    }
+}
